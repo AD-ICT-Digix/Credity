c01e4a6eb6a2dceea3578d67cf6a6f975f717fde
Nah-Nova <noaheutz@hotmail.nl>
2022-12-01 17:04:21 +0100

    Translated index

--- apps/web/pages/index.js
+++ apps/web/pages/index.js
@@ -1,18 +1,16 @@
 import React from "react";
 
-import { Card, Drawer, Header } from "../components";
-
 export default function Home() {
   const [isOpen, setIsOpen] = React.useState(false);
 
   return (
-      <div className="flex flex-col items-center justify-center w-full h-screen">
+      <div className="flex flex-col w-full h-screen">
         <h1 className="text-6xl font-bold text-center text-white">
-          Welkom bij de{" "}
+          Welcome to{" "}
           <span className="text-indigo-500">Credity</span>
         </h1>
         <h2 className="text-2xl font-bold text-center text-white">
-          De tool om data te verzamelen op de beurs
+          The tool for your event
         </h2>
       </div>
   );
