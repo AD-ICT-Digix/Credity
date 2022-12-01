9ae211bd92e4b225ff7f292ceb8063c5c3916982
Nah-Nova <noaheutz@hotmail.nl>
2022-12-01 16:30:46 +0100

    Adds basic pages

--- apps/web/pages/stand/dashboard.js
+++ apps/web/pages/stand/dashboard.js
@@ -1,11 +1,13 @@
 import React from "react";
 
-import { Card, Drawer, Header } from "../components";
+import { Card, Drawer, Header } from "../../components";
 
-export default function Home() {
+export default function StandDashboard() {
   const [isOpen, setIsOpen] = React.useState(false);
 
   return (
+    <main className="">
+      <Header setIsOpen={setIsOpen} />
       <div className="flex flex-col items-center justify-center w-full h-screen">
         <h1 className="text-6xl font-bold text-center text-white">
           Welkom bij de{" "}
@@ -15,5 +17,16 @@ export default function Home() {
           De tool om data te verzamelen op de beurs
         </h2>
       </div>
+      <Drawer isOpen={isOpen} setIsOpen={setIsOpen}>
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+      </Drawer>
+    </main>
   );
 }
