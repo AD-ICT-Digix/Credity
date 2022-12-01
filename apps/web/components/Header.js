04e8fbc2e24c43dce386dc7f0fe55e4c9da6fcf4
Nah-Nova <noaheutz@hotmail.nl>
2022-12-01 17:04:10 +0100

    Changes components

--- apps/web/components/Header.js
+++ apps/web/components/Header.js
@@ -5,7 +5,7 @@ export const Header = ({ setIsOpen }) => {
     <header className="flex justify-between p-4">
       <h1 className="font-medium">Example of a drawer</h1>
       <button
-        className="bg-green-600 text-white rounded px-4 py-1"
+        className="bg-indigo-600 text-white rounded px-4 py-1"
         onClick={() => setIsOpen(true)}
       >
         open
--- apps/web/components/Layout.js
+++ apps/web/components/Layout.js
@@ -1,5 +1,6 @@
 import Link from 'next/link';
 import { useRouter } from 'next/router';
+import FeatherIcon from 'feather-icons-react';
 
 export const Layout = ({ children }) => {
 	const router = useRouter();
@@ -7,25 +8,51 @@ export const Layout = ({ children }) => {
 		{
 			href: '/',
 			title: 'Homepage',
+			icon: 'home',
+		},
+		{
+			href: '/expo/dashboard',
+			title: 'Expo Dashboard',
+			icon: 'bar-chart',
+		},
+		{
+			href: '/expo/form',
+			title: 'Expo Form',
+			icon: 'check-square',
+		},
+		{
+			href: '/stand/dashboard',
+			title: 'Stand Dashboard',
+			icon: 'pie-chart',
+		},
+		{
+			href: '/stand/form',
+			title: 'Stand Form',
+			icon: 'check-circle',
 		},
 	];
 
 	return (
 		<div className='min-h-screen flex flex-col'>
-			<header className='bg-black sticky top-0 h-14 flex justify-center items-center font-semibold uppercase'>
+			{/* TODO: subnavigation */}
+			{/* <header className='bg-black sticky top-0 h-14 flex justify-center items-center font-semibold uppercase'>
 				Credity
-			</header>
+			</header> */}
 			<div className='flex flex-col md:flex-row flex-1' >
 				<aside className='bg-black-500 w-full md:w-60 '>
 					<nav>
 						<ul>
-							{menuItems.map(({ href, title }) => (
+							{menuItems.map(({ href, title, icon }) => (
 								<li className='m-2' key={title}>
 									<Link href={href}
 										className={`flex p-2 bg-black-200 rounded hover:bg-indigo-400 cursor-pointer ${router.asPath === href && 'bg-indigo-600 text-white'
 											}`}
 									>
-										{title}
+										{/* div that colum row space between */}
+										<div className='flex flex-1 justify-between'>
+											<span className='text-md'>{title}</span>
+											<FeatherIcon icon={icon} />
+										</div>
 									</Link>
 								</li>
 							))}
