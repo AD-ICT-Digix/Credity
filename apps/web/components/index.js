9781fca191f899b131ac4011116edd0a350e4bfa
Nah-Nova <noaheutz@hotmail.nl>
2022-12-01 16:32:27 +0100

    Adds index for components

---
+++ apps/web/components/index.js
@@ -0,0 +1,6 @@
+export { Card } from "./Card";
+export { Drawer } from './Drawer';
+export { Form } from './Form'
+export { Header } from './Header';
+export { Layout } from './Layout'
+export { Loader } from './Loader'
