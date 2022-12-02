935f7c1a79b2c3af97ebc8ddadd2c2aa86ec857f
Nah-Nova <noaheutz@hotmail.nl>
2022-12-02 11:17:48 +0100

    Updates index

--- apps/web/components/index.js
+++ apps/web/components/index.js
@@ -3,5 +3,6 @@ export { Drawer } from './Drawer';
 export { Form } from './Form'
 export { Header } from './Header';
 export { Layout } from './Layout'
+export { LineChart } from './LineChart';
 export { Loader } from './Loader'
 export { Navigator } from './Navigator'
