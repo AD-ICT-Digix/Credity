dd38a41a2eed732aab0c10bd2d111907f16b0b5c
Nah-Nova <noaheutz@hotmail.nl>
2022-12-05 18:07:52 +0100

    Adds statcard component

--- apps/web/pages/index.js
+++ apps/web/pages/index.js
@@ -1,17 +1,7 @@
 import React from "react";
 
 export default function Home() {
-  const [isOpen, setIsOpen] = React.useState(false);
-
   return (
-      <div className="flex flex-col w-full h-screen">
-        <h1 className="text-6xl font-bold text-center text-white">
-          Welcome to{" "}
-          <span className="text-indigo-500">Credity</span>
-        </h1>
-        <h2 className="text-2xl font-bold text-center text-white">
-          The tool for your event
-        </h2>
-      </div>
+    <></>
   );
 }
